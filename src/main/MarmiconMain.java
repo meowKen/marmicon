cfede2e2cd94500e20f8d1973d56e7ed5e217ea7
Stéphane LC <34244103+naked-chicken@users.noreply.github.com>
2018-05-18 12:17:21 +0200

    main

--- src/main/MarmiconMain.java
+++ src/main/MarmiconMain.java
@@ -1,6 +1,7 @@
 package main;
 
 import swing.RecepteurSaisieInstruction;
+import swing.GateauFenetre;
 import swing.RecepteurSaisieGateau;
 import swing.RecepteurSaisieIngredients;;
 
@@ -9,8 +10,9 @@ public class MarmiconMain {
 	public static void main(String[] args) {
 		//RecepteurSaisieIngredients rsing = new RecepteurSaisieIngredients();
 		//RecepteurSaisieInstruction rsins = new RecepteurSaisieInstruction();
-		RecepteurSaisieGateau rsg = new RecepteurSaisieGateau();
-		
+		//RecepteurSaisieGateau rsg = new RecepteurSaisieGateau();
+		GateauFenetre gf = new GateauFenetre();
+		gf.setVisible(true);
 	}
 
 }
